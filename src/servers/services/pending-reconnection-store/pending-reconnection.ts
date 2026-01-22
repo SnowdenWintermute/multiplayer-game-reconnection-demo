ae5b77eacfd0f0766a8ddfb53ef8844d7c8bb447
Mike <michael.p.silverman@gmail.com>
2026-01-22 13:09:09 -0500

    edit PendingReconnection in advance of renaming

--- src/servers/services/pending-reconnection-store/pending-reconnection.ts
+++ src/servers/services/pending-reconnection-store/pending-reconnection.ts
@@ -1,19 +1,15 @@
 import {
-  ConnectionId,
   GameName,
   GameServerName,
   GuestSessionReconnectionToken,
-  Username,
 } from "../../../aliases.js";
 import { ERROR_MESSAGES } from "../../../error-messages.js";
-import { GameRegistry } from "../../game-registry/index.js";
 import { UserSession } from "../../sessions/user-session.js";
 import { TaggedUserId } from "../identity-provider/tagged-user-id.js";
 
 export class PendingReconnection {
   constructor(
     public readonly taggedUserId: TaggedUserId,
-    private username: Username,
     public readonly gameName: GameName,
     public readonly gameServerName: GameServerName,
     public readonly guestUserReconnectionTokenOption: null | GuestSessionReconnectionToken
@@ -26,19 +22,9 @@ export class PendingReconnection {
 
     return new PendingReconnection(
       session.taggedUserId,
-      session.username,
       session.currentGameName,
       gameServerName,
       session.getGuestReconnectionTokenOption()
-    );
-  }
-
-  toUserSession(connectionId: ConnectionId, gameRegistry: GameRegistry) {
-    return new UserSession(
-      this.username,
-      connectionId,
-      this.taggedUserId,
-      gameRegistry
     );
   }
 }
